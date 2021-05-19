82fa22448c040d910dd3214f7ed7556d5fb27e73
henSetiawan <hendystyawan74@gmail.com>
2021-05-19 21:22:14 +0700

    add css and some new

--- src/components/CoinItem.js
+++ src/components/CoinItem.js
@@ -1,18 +1,24 @@
+import classes from "../assets/coinItem.module.css";
 const CoinItem = (props) => {
-  return <div>
-      <div>
-          <img src={props.img} alt={props.name} />
-          <h1>{props.name}</h1>
-          <p>{props.symbol}</p>
+  return (
+    <div className={classes.row}>
+      <div className={classes.col}>
+        <img src={props.img} className={classes.image} alt={props.name} />
+        <h1 className={classes.coin_name}>{props.name}</h1>
+        <p className={classes.coin_symbol}>{props.symbol}</p>
       </div>
-      <div>
-          <p>{props.price}</p>
-          <p>{props.volume}</p>
-          <p>{props.percent}</p>
-          <p>{props.marketcap}</p>
+      <div className={classes.col}>
+        <p>$ {props.price}</p>
+        <p>$ {props.volume.toLocaleString()}</p>
+        {props.percent < 0 ? (
+          <p className={classes.red}>{props.percent.toFixed(2)} %</p>
+        ) : (
+          <p className={classes.green}>{props.percent.toFixed(2)} %</p>
+        )}
+        <p>$ {props.marketcap.toLocaleString()}</p>
       </div>
-  </div>;
+    </div>
+  );
 };
-
 
 export default CoinItem;
